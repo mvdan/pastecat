c40c53eaa679a871e58d547ba871f1d7f7503237
Daniel Martí <mvdan@mvdan.cc>
2015-01-21 10:09:21 -0800

    Revert "Move MaxBytesReader into helper func"
    This reverts commit bb14de377a0cd687bd8eb96bbb6e1eee2ac55110.

--- pastecat.go
+++ pastecat.go
@@ -110,7 +110,6 @@ func describeLimits() string {
 }
 
 func getContentFromForm(r *http.Request) (content []byte, err error) {
-	r.Body = http.MaxBytesReader(w, r.Body, int64(maxSize))
 	if value := r.FormValue(fieldName); value != "" {
 		return []byte(value), nil
 	}
@@ -162,6 +161,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		http.ServeContent(w, r, "", header.ModTime, content)
 
 	case "POST":
+		r.Body = http.MaxBytesReader(w, r.Body, int64(maxSize))
 		content, err := getContentFromForm(r)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
