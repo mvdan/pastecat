06076ca85620f4ee7900b15b26f02d5787720460
Daniel Martí <mvdan@mvdan.cc>
2015-01-25 18:38:49 -0800

    Add first test for stats.go

---
+++ stats_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMakeSpaceFor(t *testing.T) {
+	for _, c := range [...]struct {
+		inNumber     int
+		inMaxNumber  int
+		inStorage    int64
+		inMaxStorage int64
+		inSize       int64
+		want         error
+	}{
+		{0, 0, 0, 0, 1, nil},
+		{0, 5, 0, 5, 3, nil},
+		{0, 1, 0, 1, 1, nil},
+		{1, 1, 0, 1, 1, ErrReachedMaxNumber},
+		{5, 1, 0, 1, 1, ErrReachedMaxNumber},
+		{0, 1, 1, 1, 1, ErrReachedMaxStorage},
+		{0, 1, 5, 1, 3, ErrReachedMaxStorage},
+	} {
+		stats := Stats{
+			number:     c.inNumber,
+			maxNumber:  c.inMaxNumber,
+			storage:    c.inStorage,
+			maxStorage: c.inMaxStorage,
+		}
+		got := stats.makeSpaceFor(c.inSize)
+		if got != c.want {
+			t.Errorf(`%+v.makeSpaceFor(%v) didn't error as expected.`, stats, c.inSize)
+		}
+	}
+}
