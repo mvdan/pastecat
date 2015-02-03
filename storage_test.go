1780fe06ed864a16ac135ae15dab7f34c9be7a28
Daniel Martí <mvdan@mvdan.cc>
2015-02-02 18:04:53 -0800

    Run go fmt

--- storage_test.go
+++ storage_test.go
@@ -4,14 +4,13 @@ import (
 	"testing"
 )
 
-
 func TestRandomID(t *testing.T) {
 	for _, c := range []struct {
 		available func(ID) bool
 		wantErr   bool
 	}{
-		{func(id ID) bool { return true; }, false},
-		{func(id ID) bool { return false; }, true},
+		{func(id ID) bool { return true }, false},
+		{func(id ID) bool { return false }, true},
 	} {
 		_, err := randomID(c.available)
 		if c.wantErr {
